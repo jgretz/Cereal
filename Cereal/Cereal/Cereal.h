e6cef7566f308e9d6eccf9c717bca28acf3a9011
jgretz <jgretz@truefitsolutions.com>
2013-05-18 09:55:10 -0400

    initial project

---
+++ Cereal/Cereal/Cereal.h
@@ -0,0 +1,13 @@
+//
+//  Cereal.h
+//  Cereal
+//
+//  Created by Joshua Gretz on 5/18/13.
+//  Copyright (c) 2013 TrueFit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cereal : NSObject
+
+@end
