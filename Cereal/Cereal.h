d0cb7d6fa0f3d6f844aab4023f1e48d7cbdf02a9
jgretz <jgretz@truefitsolutions.com>
2015-10-30 16:57:58 -0400

    initial project

---
+++ Cereal/Cereal.h
@@ -0,0 +1,19 @@
+//
+//  Cereal.h
+//  Cereal
+//
+//  Created by Joshua Gretz on 10/30/15.
+//  Copyright © 2015 Truefit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cereal.
+FOUNDATION_EXPORT double CerealVersionNumber;
+
+//! Project version string for Cereal.
+FOUNDATION_EXPORT const unsigned char CerealVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cereal/PublicHeader.h>
+
+
---
+++ CerealTests/CerealTests.swift
@@ -0,0 +1,36 @@
+//
+//  CerealTests.swift
+//  CerealTests
+//
+//  Created by Joshua Gretz on 10/30/15.
+//  Copyright © 2015 Truefit. All rights reserved.
+//
+
+import XCTest
+@testable import Cereal
+
+class CerealTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
